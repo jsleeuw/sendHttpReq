884272e49250b2e1ed0186171e3aeb391e58bdc7
jonathan sleeuw <jsleeuw@gmail.com>
2024-11-08 11:02:39 -0500

    ditto

--- src/main/java/com/example/HttpPostExample.java
+++ src/main/java/com/example/HttpPostExample.java
@@ -7,8 +7,8 @@ import java.nio.charset.StandardCharsets;
 
 public class HttpPostExample {
   public static void main(String[] args) throws Exception {
-    String url = "YOUR_URL_HERE";
-    String jsonInputString = "YOUR_JSON_HERE";
+    String url = "http://localhost:9999/rpk/post";
+    String jsonInputString = "[{\"underlying\":\"NVDA\",\"symbol\":\"NVDA241108C00050000\",\"bidPrice\":97.75,\"askPrice\":98.25,\"bidSize\":26,\"askSize\":25,\"timestamp\":\"2024-11-07T17:16:16.000Z\"}]";
 
     URL obj = new URL(url);
     HttpURLConnection con = (HttpURLConnection) obj.openConnection();
